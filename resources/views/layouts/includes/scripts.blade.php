bb844d9135f70a7ff9762c0125e1192ea135b3c0
Narvijay <nv@culture-red.com>
2023-01-09 11:24:45 +0530

    login page styling added

--- resources/views/layouts/includes/scripts.blade.php
+++ resources/views/layouts/includes/scripts.blade.php
@@ -1,3 +1,4 @@
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/jquery.min.js') }}"></script>
 <script src="{{ asset('js/datatables.min.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
