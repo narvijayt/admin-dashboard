5509e1d057575af98907c5adb6dbf3eb4c768a1f
Narvijay <nv@culture-red.com>
2023-01-09 13:29:49 +0530

    Dashboard Styling

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -1,17 +1,17 @@
-<div class="sidebar-nav sidebar-dark px-3">
+<div class="sidebar-nav sidebar-dark px-0 box-shadow-none">
     <!-- Brand Logo -->
     <a href="javascript:;" class="brand-link px-3">
         <span class="brand-text font-weight-light"><b> {{ env('APP_NAME', 'Admin Dashboard')}}</b></span>
     </a>
     <div class="sidebar">
         <ul class="nav flex-column">
-            <li class="nav-item"><a class="nav-link {{ (getControllerName() == 'DashboardController') ? 'active' : '' }}" href="{{ route('dashboard') }}">Dashboard</a></li>
+            <li class="nav-item mb-1 active "><a class="nav-link px-3 {{ (getControllerName() == 'DashboardController') ? 'active' : '' }}" href="{{ route('dashboard') }}"><i class="icon-dashboard me-2"></i> Dashboard</a></li>
             @if( in_array(strtolower(session()->get('accountType')), ['admin']) )
-                <li class="nav-item"><a class="nav-link" href="{{ route('participants') }}">Participants</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('users') }}">Users</a></li>
+                <li class="nav-item mb-1"><a class="nav-link px-3" href="{{ route('participants') }}"><i class="icon-list me-2"></i>Participants</a></li>
+                <li class="nav-item mb-1"><a class="nav-link px-3" href="{{ route('users') }}"><i class="icon-user me-2"></i>Users</a></li>
             @endif
             @if(in_array(strtolower(session()->get('accountType')), ['admin', 'translator']) )
-                <li class="nav-item"><a class="nav-link {{ (getControllerName() == 'TranslationController') ? 'active' : '' }}" href="{{ route('translations') }}">Translations</a></li>
+                <li class="nav-item mb-1"><a class="nav-link px-3 {{ (getControllerName() == 'TranslationController') ? 'active' : '' }}" href="{{ route('translations') }}"><i class="icon-edit me-2"></i>Translations</a></li>
             @endif
         </ul>
     </div>
