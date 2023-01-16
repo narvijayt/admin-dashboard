4deb4c200ea3ba982928608ec69a63882df4f154
Narvijay <nv@culture-red.com>
2023-01-16 16:38:07 +0530

    dashboard style updated

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,11 +1,35 @@
-<nav class="navbar navbar-expand navbar-white navbar-light main-header sticky-top">
+<nav class="navbar navbar-expand navbar-white navbar-light main-header sticky-top ">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
-        <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-        </li>
-    </ul>
+    <ul class="navbar-nav w-100 px-4 align-items-center">
+   <li class="nav-item">
+     <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+       <i class="fas fa-bars"></i>
+     </a>
+   </li>
+   <li class="nav-item d-none d-sm-inline-block">
+     <a href="index3.html" class="nav-link">Home</a>
+   </li>
+   <li class="nav-item logout-menu">
+     <div class="dropdown">
+       <a class="dropdown-toggle text-dark d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">  
+		   <div class="rounded text-white bg-primary logout-dropdown">
+		    A 
+		   </div>
+	   </a>
+       <ul class="dropdown-menu shadow animated--grow-in" aria-labelledby="dropdownMenuButton1">
+         <li>
+           <a class="dropdown-item" href="#">
+             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile </a>
+         </li>
+         <li>
+           <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> Logout </a>
+         </li>
+       </ul>
+     </div>
+   </li>
+ </ul>
+			 
+	
+	
 </nav>
